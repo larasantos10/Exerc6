04ded22f3c43097cbb64e83a22cfd0fb9a310d6e
ifnmg <ifnmg@192.168.163.33>
2019-05-20 21:51:40 -0300

    Exerc6

---
+++ src/Main/TestaPessoa.java
@@ -0,0 +1,17 @@
+
+package Escola;
+
+public class TestaPessoa {
+        public static void main(String[] args){
+        
+        Pessoa a1 = new Pessoa("Lara","Rua: J","(38) 9 98834-3931");
+        
+        
+        System.out.println("Nome: "+a1.getNome());
+        System.out.println("Matricula: "+a1.getEndereco());
+        System.out.println("Matricula: "+a1.getTelefone());
+        
+      
+        
+    }
+}
